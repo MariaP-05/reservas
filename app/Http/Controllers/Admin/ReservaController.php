a1ba9c5362c5b6c96b8e2737d9afc6cd5ab601ac
Federico Veiga HP <fede.veiga14@gmail.com>
2025-01-17 11:25:30 -0300

    primera etapa calendario

--- app/Http/Controllers/Admin/ReservaController.php
+++ app/Http/Controllers/Admin/ReservaController.php
@@ -44,25 +44,23 @@ class ReservaController extends Controller
 
     public function store(Request $request)
     {
-       
+
         try {
             $reserva = new Reserva($request->all());
-            if($request->nombre_cliente !== '' && $request->nombre_cliente !== null)
-            {
+            if ($request->nombre_cliente !== '' && $request->nombre_cliente !== null) {
                 $cliente = new Cliente();
                 $cliente->nombre = $request->nombre_cliente;
                 $cliente->telefono = $request->telefono_cliente;
-                 
+
                 $cliente->save();
-              
+
                 $reserva->id_cliente = $cliente->id;
             }
-            if( $reserva->id_estado_reserva == '')
-            {
+            if ($reserva->id_estado_reserva == '') {
                 $reserva->id_estado_reserva = 1;
             }
-            
-            
+
+
             $reserva->valor = $request->recargo +  $request->total;
             $reserva->save();
 
@@ -113,8 +111,7 @@ class ReservaController extends Controller
             ->orderby('fecha_desde', 'desc')->first();
         $cant_dias = $fecha_desde->diffInDays($fecha_hasta);
 
-        if(isset($precio_entrada) )
-        {
+        if (isset($precio_entrada)) {
             $reserva->importe_reserva =  $precio_entrada->valor * $cant_dias;
 
             $reserva->descuento_porce = (100 * $reserva->descuento) / $reserva->importe_reserva;
@@ -122,7 +119,7 @@ class ReservaController extends Controller
             $reserva->recargo_porce = (100 * $reserva->recargo) / $reserva->total;
             $reserva->total_deuda = $reserva->total + $reserva->recargo - $reserva->senia;
         }
-       
+
 
         return view('admin.reservas.edit', compact('reserva', 'clientes', 'cabanias', 'formas_pago',  'estado_reservas'));
     }
@@ -140,16 +137,14 @@ class ReservaController extends Controller
             $reserva = Reserva::findOrFail($id);
 
             $reserva->id_cabania = $request->id_cabania;
-            $reserva->id_cliente = $request->id_cliente; 
- 
-            if($request->nombre_cliente !== '' && $request->nombre_cliente !== null)
-            {
+            $reserva->id_cliente = $request->id_cliente;
+
+            if ($request->nombre_cliente !== '' && $request->nombre_cliente !== null) {
                 $cliente = new Cliente();
                 $cliente->nombre = $request->nombre_cliente;
                 $cliente->telefono = $request->telefono_cliente;
                 $cliente->save();
                 $reserva->id_cliente = $cliente->id;
-                
             }
             $reserva->id_forma_pago = $request->id_forma_pago;
             $reserva->ctabancaria = $request->ctabancaria;
@@ -225,22 +220,105 @@ class ReservaController extends Controller
     public function calendario(Request $request)
     {
         $reservas = Reserva::all();
+        $cabanias = Cabania::all();
 
         $fecha_desde = new Carbon();
         $fecha_desde->firstOfMonth();
 
         $fecha_hasta = new Carbon();
+        $fecha_hasta->firstOfMonth();
+        // $fecha_hasta->addMonths(1);
         $fecha_hasta->lastOfMonth();
 
+
         $dias = [];
-       
-        while($fecha_desde <= $fecha_hasta)
-        {
-           
-            $dias[] =new Carbon( $fecha_desde);
-            $fecha_desde->addDays(1); 
+        $months = [];
+        $span = [];
+        $i = 0;
+        $k = 0;
+        while ($fecha_desde <= $fecha_hasta) {
+            $mes = $fecha_desde->locale('es_Ar')->isoFormat('MMMM');
+            if (in_array($mes, $months) == false) {
+                $span[] = $i * 2;
+                $months[] =  $fecha_desde->locale('es_Ar')->isoFormat('MMMM');
+                $i = 1;
+            } else {
+                $i++;
+            }
+
+            $dias[] = new Carbon($fecha_desde);
+            $fech_hast = new Carbon($fecha_desde);
+            $fech_hast->addDays(1);
+            $dia_compartido = 0;
+            foreach ($cabanias as $cabania) {
+                $reserva = Reserva::where('id_cabania', $cabania->id)
+                    //->where('fecha_desde','>=',$fecha_desde->format('Y-m-d'))
+                    ->where('fecha_desde', '<=', $fecha_desde->format('Y-m-d'))
+                    ->where('fecha_hasta', '>=', $fech_hast->format('Y-m-d')) //->first();
+                    ->first();
+                //  $cabania->reserva[$k] = $reserva;
+                if (isset($reserva)) {
+                    $comienzo = new Carbon($reserva->fecha_desde);
+                    $fin = new Carbon($reserva->fecha_hasta);
+                    $reserva->span = $comienzo->diffInDays($fin) * 2;
+
+                    //dd($reserva->fecha_hasta , $fech_hast->format('Y-m-d'));
+
+                    if ($reserva->fecha_hasta == $fech_hast->format('d-m-Y')) {
+
+                        $reserva->span = $reserva->span + 1;
+                        $dia_compartido = 1;
+                    }
+                    if ($reserva->fecha_desde == $fecha_desde->format('d-m-Y')) {
+
+                        if($k == 0)
+                        {
+                            $reserva_2 = new Reserva();
+                            $reserva_2->span = 1;
+                            $cab[$cabania->id][$k] = $reserva_2;
+                            $k++;
+                            // dd($reserva->fecha_desde,  $cab);
+                        }
+                        $reserva->span = $reserva->span - 1;
+                        $dia_compartido = 2;
+                    }
+                } else {
+                    $reserva = new Reserva();
+                    if ($dia_compartido == 1) {
+                        $reserva->span = 1;
+                        $dia_compartido = 0;
+                    } else {
+                        if ($dia_compartido == 2) {
+                            $reserva->span = 3;
+                            $dia_compartido = 0;
+                        } else {
+                            $reserva->span = 2;
+                        }
+                    }
+                }
+
+
+                $cab[$cabania->id][$k] = $reserva;
+                // dd($reserva,$cabania->id, $fecha_desde->format('Y-m-d'), $fech_hast->format('Y-m-d'));
+            }
+            $fecha_desde->addDays(1);
+            $k++;
         }
-        dd($dias, $fecha_hasta, $fecha_desde );
-        return view('admin.reservas.calendario', compact('reservas','fecha_desde', 'fecha_hasta'));
+//dd($cab, $span);
+        $span[] = $i * 2;
+        array_shift($span);
+        //dd($span );
+        $t = 0;
+        return view('admin.reservas.calendario', compact(
+            'cab',
+            'span',
+            't',
+            'months',
+            'dias',
+            'cabanias',
+            'reservas',
+            'fecha_desde',
+            'fecha_hasta'
+        ));
     }
 }
--- resources/views/admin/reservas/calendario.blade.php
+++ resources/views/admin/reservas/calendario.blade.php
@@ -3,108 +3,76 @@
 @section('title', 'Lista de Reservas')
 
 @section('content_header')
-    <h1>Reservas</h1>
+<h1>Reservas</h1>
 @stop
 
 @section('content')
-    <div class="card">
-        <a href="{{ route('admin.reservas.create') }}" title="Crear Nueva Reserva"
-            style="position:fixed;	width:60px;	height:60px; top:57px;	right:40px;
+<div class="card">
+    <a href="{{ route('admin.reservas.create') }}" title="Crear Nueva Reserva"
+        style="position:fixed;	width:60px;	height:60px; top:57px;	right:40px;
 background-color:#FFF;	color:#25d366;	border-radius:50px;	text-align:center;
 font-size:30px;	box-shadow: 2px 2px 3px #999; z-index:100;"
-            target="_blank" onMouseOver="this.style.color='#FFF'; this.style.background = '#25d366'"
-            onMouseOut="this.style.color='#25d366'; this.style.background = '#fff'">
-            <i class="fa fa-plus" style="margin-top:16px"></i>
-        </a>
+        target="_blank" onMouseOver="this.style.color='#FFF'; this.style.background = '#25d366'"
+        onMouseOut="this.style.color='#25d366'; this.style.background = '#fff'">
+        <i class="fa fa-plus" style="margin-top:16px"></i>
+    </a>
 
-        <div class="cadr-body">
-            <div class="form-group col-sm-12">
-                <div class="row">
-                    <br>
-                </div>
-                <table id="reservas" class="table table-striped col-sm-12">
-                    <thead class="bg-secondary text-white">
-                        <tr>
-                            <th>Id</th>
-                            <th>Cabaña</th>
-                            <th>Cliente</th>
-                            <th>Fecha desde</th>
-                            <th>Fecha hasta</th>
-                            <th>Estado</th>
-                            <th>Importe</th>
-                            <th>Acciones</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($reservas as $reserva)
-                            <tr>
-                                <td>{{ $reserva->id }}</td>
-                                <td>{{ isset($reserva->Cabania) ? $reserva->Cabania->denominacion : '' }}</td>
-                                <td>{{ isset($reserva->Cliente) ? $reserva->Cliente->nombre : '' }}</td>
-                                <td>{{ $reserva->fecha_desde }}</td>
-                                <td>{{ $reserva->fecha_hasta }}</td>                               
-                                <td>{{ isset($reserva->Estado_reserva) ? $reserva->Estado_reserva->denominacion : '' }}</td>
-                                <td>{{ $reserva->valor }}</td>                 
-                                <td>
-                                    <div class="row col-sm-12">
-                                        <div class="col-md-4 form-group">
-                                            <form method="post" action="{{ route('admin.reservas.destroy', $reserva->id) }}">
-                                                @method('delete')
-                                                @csrf
-                                                <button type="submit" class="btn btn-outline-danger"
-                                                    title="Eliminar Reserva">
-                                                    <i class="fa fa-trash"></i>
-                                                </button>
-                                            </form>
-                                        </div>
-                                        <div class="col-md-4 form-group">
-                                            <form method="get" action="{{ route('admin.reservas.edit', $reserva->id) }}">
-
-                                                <button type="submit" class="btn btn-outline-primary"
-                                                    title="Editar Reserva">
-                                                    <i class="fa fa-edit"></i>
-                                                </button>
-                                            </form>
-                                        </div>
-                    
-                                        <div class="col-md-4 form-group">
-                                            @php
-                                                    $reserva->deno_cabania = isset($reserva->Cabania)
-                                                    ? $reserva->Cabania->denominacion : '';
-
-                                                    $reserva->nom_cliente = isset($reserva->Cliente)
-                                                    ? $reserva->Cliente->nombre : '';
-
-                                                    $reserva->deno_pago = isset($reserva->Forma_pago)
-                                                    ? $reserva->Forma_pago->denominacion : '';
-
-                                                    $reserva->deno_est_reserva = isset($reserva->Estado_reserva)
-                                                    ? $reserva->Estado_reserva->denominacion : '';
-                                            @endphp
-                                            <button type="button" class="btn btn-outline-warning"
-                                                title="Ver datos reserva" onMouseOver="this.style.color='#FFF'"
-                                                onMouseOut="this.style.color= '#fa7101'" data-toggle="modal"
-                                                data-target="#VerModal" data-whatever="{{ $reserva }}">
-                                                <i class="fa fa-eye"></i>
-                                            </button>
-                                        </div>
-                                    </div>
-                                </td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
+    <div class="cadr-body">
+        <div class="form-group col-sm-12">
+            <div class="row">
+                <br>
             </div>
-            @include('admin.reservas.partials.ver')
+            <table id="reservas" class="table table-striped col-sm-12">
+                <thead class="bg-secondary text-white">
+                    <tr>
+                    <th colspan="1" style = "border-right: solid 2px; border-color:#999;"></th>
+                        @foreach ($months as $month)
+                        <th style = "border-right: solid 2px; border-color:#999;" colspan="{{$span[$t]}}">{{ucfirst($month) }}</th>
+                        @endforeach
+                    </tr>
+                    <tr>
+                        <th style = "border-right: solid 2px; border-color:#999;" colspan="1">Cabanias </th>
+                        @foreach ($dias as $dia)
+                        <th style = "border-right: solid 2px; border-color:#999;" colspan="2">{{$dia->format('d') }}</th>
+                        @endforeach
+                    </tr>
+                </thead>
+                <tbody>
+                    @php
+                    $id_reserva = 0;
+                    @endphp
+                    @foreach ($cabanias as $cabania)
+                    <tr>
+                        <td style = "border-right: solid 2px; border-color:#999;" colspan="1">{{ $cabania->denominacion }}</td>
+                        @foreach ($cab[$cabania->id] as $reserva)
+                            @if(isset($reserva->id))
+                                @if($reserva->id !== $id_reserva)                                    
+                                    <td style = "border-right: solid 2px; border-color:#999;" colspan="{{$reserva->span}}">{{isset($reserva->Cliente) ? $reserva->Cliente->nombre : ''}} </td>
+                                    @php
+                                        $id_reserva = $reserva->id;
+                                    @endphp
+                                @endif
+                            @else
+                                @if(isset($reserva->span))
+                                <td style = "border-right: solid 2px; border-color:#999;" colspan="{{$reserva->span}}">s </td>
+                                @else
+                                    <td style = "border-right: solid 2px; border-color:#999;" colspan="2">t </td>
+                                @endif
+                            @endif
+                        @endforeach
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
         </div>
+        @include('admin.reservas.partials.ver')
+    </div>
     @stop
 
     @section('css')
-    
+
     @stop
 
     @section('js')
-    
-    <script src="{{ asset('admin1/reservas/index.js') }}"></script>
- 
+
     @stop
