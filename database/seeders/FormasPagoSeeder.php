b0e0b72c8340e2a1cb6e81b5918946416e02e876
federico veiga <fede.veiga14@gmail.com>
2026-04-07 16:50:19 -0300

    add ingressos egresoso

--- database/seeders/FormasPagoSeeder.php
+++ database/seeders/FormasPagoSeeder.php
@@ -16,7 +16,7 @@ class FormasPagoSeeder extends Seeder
     public function run()
     {
         //
-      /*  Forma_pago::create([
+        Forma_pago::create([
             'denominacion' => 'Transferencias'
         ] );
 
@@ -34,7 +34,7 @@ class FormasPagoSeeder extends Seeder
 
         Forma_pago::create([
             'denominacion' => 'Crédito Tarjeta'
-        ] );*/
+        ] );
          Forma_pago::create([
             'denominacion' => 'AIRBNB'
         ] );
